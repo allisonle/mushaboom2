7c933af4024cf6a286ed85e74cf7049418932dc8
Allison Le <allison@mashable.com>
2018-02-01 00:41:46 -0800

    descriptive commit messages are for non-personal projects

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,30 @@
-// This is where it all goes :)
+document.addEventListener("DOMContentLoaded", function() {
+  Barba.Pjax.init();
+  Barba.Prefetch.init();
+  Barba.Dispatcher.on('linkClicked', function(el) {
+    console.log("HERE WE GOOOOOOO");
+  });
+
+  var HideShowTransition = Barba.BaseTransition.extend({
+    start: function() {
+      this.newContainerLoading.then(this.movePage.bind(this));
+    },
+
+    movePage: function() {
+      $(this.oldContainer).animate({top: "-100vh"}, 600);
+      $(this.newContainer).css({
+        visibility : 'visible',
+        position: 'fixed',
+        top: '100vh'
+      });
+
+      $(this.newContainer).animate({top: '0px'}, 600, () => {
+        this.done();
+      });
+    },
+  });
+
+  Barba.Pjax.getTransition = function() {
+    return HideShowTransition;
+  };
+})
